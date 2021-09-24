c367d99c2fdf7ddae8846f953b87666636707446
Kenny Kerr <kenny@kennykerr.ca>
2021-09-24 14:25:15 -0700

    allow deref

--- src/net.rs
+++ src/net.rs
@@ -943,6 +943,7 @@ impl AcceptAddrsBuf {
         }
     }
 
+    #[allow(deref_nullptr)]
     fn args(&self) -> (*mut std::ffi::c_void, u32, u32, u32) {
         let remote_offset = unsafe { &(*(0 as *const AcceptAddrsBuf)).remote as *const _ as usize };
         (
